b7b966006006ffd58b6bb24aa5eb3fe003330dc9
Vick Palomo <ing.victorpalomo@gmail.com>
2025-07-03 22:37:53 -0600

    fix: add anotation @jsonIgnore  in domain/Document

--- src/main/java/com/clara/ops/challenge/document_management_service_challenge/domain/Document.java
+++ src/main/java/com/clara/ops/challenge/document_management_service_challenge/domain/Document.java
@@ -1,5 +1,6 @@
 package com.clara.ops.challenge.document_management_service_challenge.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
@@ -9,6 +10,7 @@ import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
@@ -69,5 +71,8 @@ public class Document {
       schema = "document_schema",
       joinColumns = @JoinColumn(name = "document_id"),
       inverseJoinColumns = @JoinColumn(name = "tag_id"))
-  private Set<Tag> tags;
+
+  @JsonIgnore
+  private Set<Tag> tags = new HashSet<>();
+
 }
